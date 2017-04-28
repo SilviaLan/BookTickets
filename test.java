5b5f3b956b756d6118157b6d89d2a8ed11b8b084
RangerX-36917 <chrischen369@outlook.com>
2017-04-28 11:18:00 +0800

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ public class test
 {
     public static void main(String [] args)
     {
-        System.out.println("This is a java program on MS Surface Pro 4 M3");
+        System.out.println("This is a java program ");
     }
 }
